9d57e0e5565619832d8c04f1b54ebfb2c64c9a9b
rukednous <luke.winikates@gmail.com>
2013-03-24 09:15:41 -0700

    add an assertion on the request body

--- spec/requests/games_controller_spec.rb
+++ spec/requests/games_controller_spec.rb
@@ -6,6 +6,7 @@ describe "games" do
       game = Game.create_default
       get "games/#{game.id}"
       expect(response).to be_success
+      expect(response.body).to eq(GameSerializer.new(game).to_json)
     end
   end
 end
