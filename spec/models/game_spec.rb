6d60cd3a7d0a073e6958905828823c38df48ea7f
rukednous <luke.winikates@gmail.com>
2013-03-31 20:43:43 -0700

    replace more tests with specs

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
 describe Game do
+  describe "rolling to start" do
+    let(:game) { Game.create_default }
+    subject { game.reload }
+
+    before { game.roll_to_start }
+    its(:current_player) { should_not be_nil }
+    its(:dice) { should_not be_nil }
+  end
+
   describe "creating a traditional game" do
     let(:game) { Game.create_with_traditional_layout }
 
@@ -23,6 +32,33 @@ describe Game do
        %w(14, 15, 16, 18, 20, 21, 22, 23)).each do |num|
         game.point(num).should have_no_checkers 
       end
+    end
+  end
+
+  describe "moving checkers" do
+    let(:game) { Game.create_default }
+
+    before do
+      game.current_player = :black
+      game.point(1).add_checker :black
+      game.dice = Dice.new [1]
+      game.move(:black, 1, 2)
+    end
+
+    it "allows moving from a space if the player has a checker there" do
+      game.can_move_from(:black, 2).should be_true
+    end
+
+    it "reduces the number of checkers at the start point" do
+      game.point(1).reload.checker_count.should == 0
+    end
+
+    it "increments the number of checkers at the end point" do
+      game.point(2).checker_count.should == 1
+    end
+
+    context "when taking a checker" do
+      
     end
   end
 end
--- test/unit/game_test.rb
+++ test/unit/game_test.rb
@@ -1,30 +1,6 @@
 require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
-  test "rolling for advantage" do
-    game = Game.create_default
-    assert !game.current_player
-    game.roll_to_start
-    assert !!game.current_player
-    assert !!game.dice
-  end
-
-  test "moving a checker" do
-    game = Game.create_default
-    game.points.find_by_num(1).add_checker :black
-    game.current_player = :black
-    game.dice = Dice.new [1]
-    game.move(:black, 1, 2)
-    assert game.points.find_by_num(2).checker_count == 1
-  end
-
-  test "can move from" do
-    game = Game.create_default
-    game.points.find_by_num(1).add_checker :black
-    game.save!
-    assert game.can_move_from(:black, 1)
-  end
-
   test "taking a checker" do
     game = Game.create_default
     game.dice = Dice.new([1, 4])
