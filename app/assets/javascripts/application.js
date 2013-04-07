79838652fc82c7b2025dd063d1665d06acd9b432
rukednous <luke.winikates@gmail.com>
2013-04-07 13:34:47 -0700

    render stuff with Ember

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,4 @@
+//= require vendor/jquery.min
+//= require vendor/handlebars
+//= require vendor/ember
+//= require gammon
--- app/assets/javascripts/views/application_view.js
+++
@@ -1,3 +0,0 @@
-Gammon.ApplicationView = Ember.View.extend({
-  templateName: 'application'
-});
